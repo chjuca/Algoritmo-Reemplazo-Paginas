063528ff940ee52de87a484e9d5a2cc2ed8b7e38
renatojobal <renatojobal@gmail.com>
2020-07-03 11:20:46 -0500

    Added initial form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { InitialFormComponent } from './components/initial-form/initial-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    InitialFormComponent,
   ],
   imports: [
     BrowserModule,
