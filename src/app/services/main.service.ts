1a2e4d8a85d08665d1082e4703290a38ebd426dc
CarlosCastillo10 <carloscastillo090916@gmail.com>
2020-07-08 16:16:54 -0500

    Eliminando dependencia 'os' que estaba generando conflictos

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { type } from 'os';
+
 
 /**
  * Main service for hold the main variables of the app, such as the
